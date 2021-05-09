7e29a890a82e0d170b141489e69234683f9208c2
Lucas Veras <lucas.veras01@gmail.com>
2021-05-09 01:10:13 -0300

    Espaçamentos

--- src/main/java/com/lucas/course/entities/User.java
+++ src/main/java/com/lucas/course/entities/User.java
@@ -72,7 +72,7 @@ public class User implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
